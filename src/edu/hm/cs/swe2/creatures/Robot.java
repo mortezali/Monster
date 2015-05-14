c462e31aa3b52fd732441291df480d053fe6a3a3
Morteza Lichtenstern <lichtens@hm.edu>
2015-05-14 11:07:19 +0200

    added Fairy, Zombie and Robot

--- src/edu/hm/cs/swe2/creatures/Robot.java
+++ src/edu/hm/cs/swe2/creatures/Robot.java
@@ -1,5 +1,40 @@
 package edu.hm.cs.swe2.creatures;
 
-public class Robot {
+public class Robot extends Creature {
+
+	private String name;
+	private final static int maxLifePoint = 6;
+	private int n;
+
+	public Robot(String name) {
+		super(name, maxLifePoint);
+		int n = n;
+
+	}
+	public void damage() {
+		int i;
+		for (i = 0; i <= 5; i++){
+			this.lifePoint = this.lifePoint - 0;{
+				for (i = 0; i > 5; i --){
+					this.lifePoint = this.lifePoint - this.lifePoint;
+				}
+			}
+		}
+	
+	}
+//	public void attack(Creature other) {
+//		int i;
+//		for (i = 0; i <= 5; i ++ ) {
+//			this.damage() = this.
+//		}
+//
+//	}
+
+
+	@Override
+	public int getInitialLivePoints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- src/edu/hm/cs/swe2/creatures/Zombie.java
+++ src/edu/hm/cs/swe2/creatures/Zombie.java
@@ -1,5 +1,29 @@
 package edu.hm.cs.swe2.creatures;
 
-public class Zombie {
+public class Zombie extends Creature {
+
+	public Zombie(String name, int maxLifePoint) {
+		super(name, maxLifePoint);
+
+	}
+
+	private String name = "";
+	private boolean alreadyDamaged;
+	private final static int maxLifePoint = 0;
+
+	@Override
+	public void attack(Creature other) {
+
+		if (this.isAlive() && other.isAlive() && this != other) {
+			other.damage();
+			other.damage();
+		}
+
+	}
+
+	@Override
+	public int getInitialLivePoints() {
+		return 0;
+	}
 
 }
